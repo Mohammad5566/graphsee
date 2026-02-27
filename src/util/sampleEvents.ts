126d5b03b9383a089ea72884add0908469302c7d
Mohammad Mahmud <mohammadmahmud5566@yahoo.com>
2026-02-27 00:07:13 -0500

    update Event interface, add sample events

--- src/util/sampleEvents.ts
+++ src/util/sampleEvents.ts
@@ -1,15 +1,18 @@
 // sample events for testing purposes
 export type Event = {
-  type: "current" | "visit";
   node: number;
+  state: "visited" | "visiting";
   timestamp: number;
+  edge?: { from: number; to: number }; // traversing edge from -> to
 };
 
 export const sampleEvents: Event[] = [
-  { type: "current", node: 0, timestamp: 0 },
-  { type: "visit", node: 0, timestamp: 1 },
-  { type: "current", node: 1, timestamp: 2 },
-  { type: "visit", node: 1, timestamp: 3 },
-  { type: "current", node: 2, timestamp: 4 },
-  { type: "visit", node: 2, timestamp: 5 },
+  { state: "visiting", node: 1, timestamp: 0 },
+  { state: "visited", node: 1, timestamp: 1 },
+  // visiting node 1 from node 0
+  { state: "visiting", node: 2, timestamp: 2, edge: { from: 1, to: 2 } },
+  { state: "visited", node: 2, timestamp: 3 },
+  // visiting node 2 from node 1
+  { state: "visiting", node: 4, timestamp: 4, edge: { from: 2, to: 4 } },
+  { state: "visited", node: 4, timestamp: 5 },
 ];
