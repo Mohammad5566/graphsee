cd0ffef0be2deffd79c6a56cd34fbb2f593f9fa9
Mohammad Mahmud <mohammadmahmud5566@yahoo.com>
2026-02-28 00:40:36 -0500

    update events interface

--- src/util/sampleEvents.ts
+++ src/util/sampleEvents.ts
@@ -3,16 +3,29 @@ export interface Event {
   node: number;
   state: "visited" | "visiting";
   timestamp: number;
+  lineNumber: number; // line number in code being executed
   edge?: { from: number; to: number }; // traversing edge from -> to
 }
 
 export const sampleEvents: Event[] = [
-  { state: "visiting", node: 1, timestamp: 0 },
-  { state: "visited", node: 1, timestamp: 1 },
+  { state: "visiting", node: 1, timestamp: 0, lineNumber: 1 },
+  { state: "visited", node: 1, timestamp: 1, lineNumber: 2 },
   // visiting node 1 from node 0
-  { state: "visiting", node: 2, timestamp: 2, edge: { from: 1, to: 2 } },
-  { state: "visited", node: 2, timestamp: 3 },
+  {
+    state: "visiting",
+    node: 2,
+    timestamp: 2,
+    lineNumber: 3,
+    edge: { from: 1, to: 2 },
+  },
+  { state: "visited", node: 2, timestamp: 3, lineNumber: 4 },
   // visiting node 2 from node 1
-  { state: "visiting", node: 4, timestamp: 4, edge: { from: 2, to: 4 } },
-  { state: "visited", node: 4, timestamp: 5 },
+  {
+    state: "visiting",
+    node: 4,
+    timestamp: 4,
+    lineNumber: 5,
+    edge: { from: 2, to: 4 },
+  },
+  { state: "visited", node: 4, timestamp: 5, lineNumber: 6 },
 ];
